ea7c3016078bb2691d97950a78e33f3be3f822a2
Sorina Buzila <soriibuzila@gmail.com>
2026-02-26 13:43:26 +0200

    Implement scroll logic after calculation
    Added scroll logic to smoothly scroll to the ResultSection after calculation.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -10,14 +10,11 @@ namespace Proiect2
             BindingContext = new CarbonViewModel();
         }
 
-        // --- NEW: Scroll Logic ---
         private async void OnCalculateClicked(object sender, EventArgs e)
         {
-            // 1. Wait a small fraction of a second to ensure the ViewModel
-            // has processed the calculation and set ResultVisible = true.
+
             await Task.Delay(100);
 
-            // 2. Scroll the view to the ResultSection frame
             await PageScrollView.ScrollToAsync(ResultSection, ScrollToPosition.Center, true);
         }
     }
